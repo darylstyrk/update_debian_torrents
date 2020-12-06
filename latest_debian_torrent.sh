3096284e61744ff13695c0ecd3866de39dfd5277
Daryl Styrk <darylstyrk@gmail.com>
2020-12-05 18:18:43 -0600

    Fixed some typos

--- latest_debian_torrent.sh
+++ latest_debian_torrent.sh
@@ -1,9 +1,9 @@
-#!/bin/bash -x
+#!/bin/bash 
 	STABLE="$(lynx --dump https://cdimage.debian.org/debian-cd/current/amd64/bt-cd/ | awk '/\.torrent$/ {print $NF}' | grep -m1 -oP "\d{2}\.\d?\.\d?")"
 	UPSTREAM="$(grep * /etc/debian_version)"
 
 check_4_new() {
-	if [[ ${$STABLE} != ${UPSTREAM} ]] ; then
+	if [[ ${STABLE} != ${UPSTREAM} ]] ; then
 		fetch_CDs
 		fetch_DVDs
 	fi
@@ -17,5 +17,4 @@ fetch_DVDs(){
 	lynx --dump https://cdimage.debian.org/debian-cd/current/amd64/bt-dvd/ | awk '/\.torrent$/ {print $NF}' | sort -u | while read; do wget -c -P ~/.torrents/ ${REPLY} ; done
 	}	
 
-fetch_CDs
-fetch_DVDs
+check_4_new
