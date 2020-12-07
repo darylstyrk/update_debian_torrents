f61f4427042dd9f79214037af3c0e8ad5633b195
Daryl Styrk <darylstyrk@gmail.com>
2020-12-06 22:48:31 -0600

    replacing grep for cat

--- latest_debian_torrent.sh
+++ latest_debian_torrent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
-	STABLE="$(lynx --dump https://cdimage.debian.org/debian-cd/current/amd64/bt-cd/ | awk '/\.torrent$/ {print $NF}' | grep -m1 -oP "\d{2}\.\d?\.\d?")"
-	UPSTREAM="$(grep * /etc/debian_version)"
+	STABLE="$(lynx --dump https://cdimage.debian.org/debian-cd/current/amd64/bt-cd/ | awk '/\.torrent$/ {print $NF}' | grep -m1 -oP "\d{2}\.\d?")"
+	UPSTREAM="$(cat /etc/debian_version)"
 
 check_4_new() {
 	if [[ ${STABLE} != ${UPSTREAM} ]] ; then
