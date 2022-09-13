16078c7fc74c98163d6121f6a2ab43045de2d2de
Daryl Styrk <darylstyrk@gmail.com>
2022-09-13 15:57:26 -0500

    Word changes

--- latest_debian_torrent.sh
+++ latest_debian_torrent.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 	STABLE="$(lynx --dump https://cdimage.debian.org/debian-cd/current/amd64/bt-cd/ | awk '/\.torrent$/ {print $NF}' | grep -m1 -oP "\d{2}\.\d?")"
-	UPSTREAM="$(</etc/debian_version)"
+	CURRENT="$(</etc/debian_version)"
 
 check_4_new() {
-	if [[ ${STABLE} != "${UPSTREAM}" ]] ; then
+	if [[ ${STABLE} != "${CURRENT}" ]] ; then
 		fetch_CDs
 		fetch_DVDs
 	fi
